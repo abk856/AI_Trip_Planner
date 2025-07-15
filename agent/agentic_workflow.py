95567f5d1eb09685ad0ec8d53974c0d0fdf0e0ee
aneesh bk <abk856@gmail.com>
2025-07-15 22:31:04 +0530

    code updated for loading the model

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -1,4 +1,3 @@
-
 from utils.model_loader import ModelLoader
 from prompt_library.prompt import SYSTEM_PROMPT
 from langgraph import StateGraph, MessageState, END, START
@@ -9,15 +8,38 @@ from langgraph.prebuilt import ToolNode, tools_codition
 class GraphBuilder:
     
     def __init__(self):
-        pass
-    
-    def agent_function(self):
-        pass
+        self.tools=[
+            # WeatherInfoTool(),
+            # PlacesInfoTool(),
+            # CalculatorTool(),
+            # CurrencyConverterTool()
+        ]
+        self.system_prompt = SYSTEM_PROMPT
+    def agent_function(self,state: MessageState):
+        """Main agent function""""
+        user_question = state["message"]
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"message": [response]}
     
     
     def build_graph(self):
-        pass
+        graph_builder=StateGraph(MessageState)
+        graph_builder.add_node("agent", self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tools))
+        graph_builder.add_edge(START, "agent")
+        graph_builder.add_codition_edge("agent",tools_codition) 
+        graph_builder.add_edge("tools", "agents")
+        graph_builder.add_edge("agent", END)
+        
+        self.graph = graph_builder.compile()
+        return self.graph
+    
     
     def __call__(self, *args, **kwargs):
-        pass
+     
+    
+    
+    
+    
     
