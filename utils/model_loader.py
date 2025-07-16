68f260cb054937d8004ff32ba82a10bd89a30caa
aneesh bk <abk856@gmail.com>
2025-07-16 23:09:25 +0530

    Created end point with stremlit frontend

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -32,12 +32,12 @@ class ModelLoader(BaseModel):
         print("LLM loading...")
         print(f"Loading model from provider: {self.model_provider}")
         if self.model_provider == "groq":
-            print("Loading LLM from Groq...")
+            print("Loading LLM from Groq..............")
             groq_api_key = os.getenv("GROQ_API_KEY")
             model_name = self.config['llm']['groq']['model_name']
             llm = ChatGroq(model_name=model_name, api_key=groq_api_key)
         elif self.model_provider == "openai":
-            print("Loading LLM from OpenAI...")
+            print("Loading LLM from OpenAI...........")
             openai_api_key = os.getenv("OPENAI_API_KEY")
             model_name = self.config['llm']['openai']['model_name']
             llm = ChatOpenAI(model_name=model_name, openai_api_key=openai_api_key)
